ac52cdd3a16984ea6ca9f680c1970a8dd6332a65
Josh Parker <joshBBparker@gmail.com>
2022-04-03 18:13:46 -0600

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // https://wallis.dev/blog/deploying-a-next-js-app-to-github-pages
+  assetPrefix: '/github-repo-wrangler/',
 }
 
 module.exports = nextConfig
